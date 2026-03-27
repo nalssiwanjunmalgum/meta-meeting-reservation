f8d155d79d1ae7a05ed48d86b8b1e7f6f357dc9e
sunghun <nokdonwithkj211@gmail.com>
2026-03-27 14:37:15 +0900

    init: init new project

---
+++ src/main/java/personal/sunghun/meta_reservation_service/MetaReservationServiceApplication.java
@@ -0,0 +1,13 @@
+package personal.sunghun.meta_reservation_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetaReservationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetaReservationServiceApplication.class, args);
+	}
+
+}
